fd476aa9f3321e0a51af43286090a6277e957d76
kwaters12 <kellywater@gmail.com>
2014-01-29 17:22:46 -0800

    First commit to IdeaSpace - my new Project Management tool.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideaspace::Application.config.secret_key_base = '739a3e1fd62ac1c79254ed848c8927f6327341cdc40b661dc713942a38d35f864024f23818a13dee1b9f5e16e6a55a85550fa63be12785aaa08be472369021b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ideaspace::Application.config.session_store :cookie_store, key: '_ideaspace_session'
